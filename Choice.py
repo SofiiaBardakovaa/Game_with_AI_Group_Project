5c10a7968aaa1a4639763591f61688bb84d9e878
Sofiia Bardakova <79498645+SofiiaBardakovaa@users.noreply.github.com>
2026-03-17 20:04:31 +0200

    Delete Choice.py

--- Choice.py
+++
@@ -1,7 +0,0 @@
-class Choice():
-    def __init__(self, move, value):
-        self.move = move
-        self.value = value
-
-    def __str__(self):
-        return self.move + ": " + str(self.value)
